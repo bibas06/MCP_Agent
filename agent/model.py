58b7d8757e54cf104d0593b6213c03c6437c3d36
bibas06 <bibassamal@gmail.com>
2026-03-16 12:19:10 +0530

    not iniial commit

--- agent/model.py
+++ agent/model.py
@@ -1,12 +1,12 @@
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_llm():
-
-    return ChatOpenAI(
-        model="gpt-4o",
+    return ChatGroq(
+        model="llama-3.3-70b-versatile",
+        api_key=os.getenv("GROQ_API_KEY"),
         temperature=0
     )
